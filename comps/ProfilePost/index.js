f8c6f76e4c11978f1a6f1cb5aad5066cf98d5c51
claudiacastonguay <claudcastonguay@gmail.com>
2020-10-23 11:10:45 -0700

    completed ProfilePost and Search comps

---
+++ comps/ProfilePost/index.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+var image = require('../../public/userpost.png');
+
+const Main = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const Container = styled.div`
+    background-color: rgba(255, 255, 255, 0.89);
+    border: 2px solid #000000;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 0px #18191F;
+    border-radius: 16px;
+    width: 100%;
+    max-height: 137px;
+    margin-left: 20px;
+    margin-right: 20px;
+    display: flex;
+    padding: 10px;
+    padding-left: 30px;
+    align-items: center;
+`;
+
+const Text = styled.div`
+    margin-left: 30px;
+    line-height: 0.6;
+`;
+
+const Title = styled.h2`
+    font-family: 'Assistant', sans-serif;
+    font-weight: 300;
+`;
+
+const Image = styled.img`
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+`;
+
+const ImageContainer = styled.div`
+    max-width: 300px;
+    max-height: 300px;
+`;
+
+const Description = styled.p`
+font-family: 'Assistant', sans-serif;
+font-weight: 300;
+`;
+
+const Price = styled.h2`
+font-family: 'Montserrat', sans-serif;
+`;
+
+const ProfilePost = ({title, description, price, image}) =>{
+    return <Main>
+
+    <Container>
+
+        <ImageContainer>
+                <Image src={image} />
+        </ImageContainer>
+
+            <Text>
+                <Title>{title}</Title>
+                <Description>{description}</Description>
+                <Price>${price}</Price>
+            </Text>
+
+        </Container>
+   
+   </Main>
+   }
+
+   ProfilePost.defaultProps = {
+       title: 'Default Title',
+       description: 'Description goes here...',
+       price: '00',
+       image: image
+   }
+   
+   
+   
+   export default ProfilePost;
