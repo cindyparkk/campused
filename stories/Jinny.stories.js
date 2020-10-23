d8e91b435c68323b4096783a2961d12b83da82fe
= <=>
2020-10-23 13:10:37 -0700

    add storybook

---
+++ stories/Jinny.stories.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Divider from '../comps/Divider';
+
+
+
+
+
+export default {
+    title: 'Example/Jinny Components',
+    component: Hr_Divider
+};
+
+export const Hr_Divider= () => <Divider />;
