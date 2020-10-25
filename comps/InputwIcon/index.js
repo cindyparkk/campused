3c6fb2e78dda9c0b178b5b5778488c176c018004
cpark3918 <cindypark31@gmail.com>
2020-10-24 23:53:03 -0700

    added input with icon

--- comps/InputwIcon/index.js
+++ comps/InputwIcon/index.js
@@ -2,35 +2,35 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 const Contain = styled.div`
-    min-width: 343px;
-    min-height: 89px;
     display: inline-flex;
-    flex-direction: column;
 `
-
-const Title = styled.h2`
-    margin-bottom: 15px;
+const Icon = styled.img`
+    position: absolute;
+    top: 20px;
+    padding: 15px;
+    min-width: 16px;
 `
 
 const Input = styled.input`
     min-height: 52px;
+    min-width: 343px;
     background: #FFFFFF;
     border: 2px solid #000000;
     box-sizing: border-box;
     border-radius: 10px;
-    padding-left: 15px;
+    padding-left: 40px;
 `
 
-const InputwIcon = ({title, placeholder}) => {
+const InputwIcon = ({icon, placeholder}) => {
     return <Contain>
-        <Title>{title}</Title>
+        <Icon src={icon}></Icon>
         <Input type="text" placeholder={placeholder}></Input>
     </Contain>
 }
 
 InputwIcon.defaultProps = {
-    title:"Title",
-    placeholder:"ex: John"
+    icon: "url(/user_icon.png}",
+    placeholder:"Placeholder"
 }
 
 export default InputwIcon;
