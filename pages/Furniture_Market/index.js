810c77c9c7f42e4fe7d83c2f037d987bb784aed5
= <=>
2020-11-03 06:53:14 -0800

    add furniture page

---
+++ pages/Furniture_Market/index.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+import Header from '../../comps/Header';
+import HeaderMenu from '../../comps/HeaderMenu';
+import Footer from '../../comps/Footer';
+
+
+
+
+
+
+
+
+export default function Furniture() {
+  
+    return   <div className="page">
+     <Header />
+     <HeaderMenu/>
+  
+  
+      <div className="footer">
+          <Footer />
+          </div>
+  
+    </div>
+   
+    
+  }
+  
