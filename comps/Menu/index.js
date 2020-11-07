7693c8ac8e6f01f3ea12ef41ec768305e32df707
claudiacastonguay <claudcastonguay@gmail.com>
2020-11-06 22:01:00 -0800

    connected filterbutton to filteropen, updated menu

--- comps/Menu/index.js
+++ comps/Menu/index.js
@@ -10,14 +10,18 @@ const Contain = styled.div`
 `
 
 const Content = styled.div`
-    display: ${props=>props.showmenu ? "inline-flex" : "none"};
+    // display: ${props=>props.showmenu ? "inline-flex" : "none"};
+    opacity: ${props=>props.showmenu ? "1" : "0"};
+    visibility: ${props=>props.hidden?props.hidden:"hidden"};
+    display: inline-flex;
+    z-index: 3;
+    transition: 0.3s;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     background-color: white;
     width: 100vw;
     height: 100vh;
-    z-index: 10;
     position: fixed;
     top: 0;
     left: 0;
@@ -27,15 +31,16 @@ const Open = styled.img`
     width: 28px;
     margin: 20px;
     cursor: pointer;
-`
+    display: inline-flex;
+`;
 
 const Close = styled.img`
     width: 28px;
     margin: 20px;
     position: fixed;
     cursor: pointer;
-    z-index: 12;
     display: ${props=>props.showclose ? "block" : "none"};
+    z-index: 4;
 `;
 
 const Item = styled.h2`
@@ -51,21 +56,26 @@ const Item = styled.h2`
 
 const Menu = ({}) => {
     const [selected, setSelected] = useState(1);
-    const [appear, setAppear] = useState(false);
+    const [hidden, setHidden] = useState(null);
 
     const [showmenu, SetMenu]  = useState(false);
     const [showclose, setClose] = useState(false);
 
     return <Contain>
-        <Open showmenu={showmenu} showclose={showclose} onClick={()=>{
+        <Open showclose={showclose} showmenu={showmenu} hidden={hidden} onClick={()=>{
             SetMenu(!showmenu);
             setClose(!showclose);
+            setHidden("visible");
+            
         }} src='/menu.png'></Open>
-        <Close showclose={showclose} showmenu={showmenu} onClick={()=>{
+        <Close showclose={showclose} showmenu={showmenu} hidden={hidden} onClick={()=>{
             SetMenu(!showmenu);
             setClose(!showclose);
+            setHidden("hidden");
         }} src='/close2.png'></Close>
-        <Content showmenu={showmenu}>
+
+
+        <Content showmenu={showmenu} showmenu={showmenu} hidden={hidden}>
             <Item selected={selected === 1} onClick={()=>{
                 setSelected(1);}}>Home</Item>
             <Item selected={selected === 2} onClick={()=>{
@@ -81,7 +91,7 @@ const Menu = ({}) => {
 }
 
 Menu.defaultProps = {
-    display: 'none'
+    
 }
 
 export default Menu;
