d327f7a32a901701555d3673e90b4c77a25d6b3e
cpark3918 <cindypark31@gmail.com>
2020-11-08 17:04:53 -0800

    edits

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -11,21 +11,9 @@ export default function Profile() {
       <Header />
       <HeaderMenu />
       <div>
-          <h1>Your Name</h1>
       </div>
       <Button bgcolor="#3DA5D9" color="#FFF" text="Make a listing"/>
       <ProfileSlider />
-      <div className="profile_contents">
-          <div>
-            <ProfilePost />
-          </div>
-          <div>
-            <ProfilePost />
-          </div>
-          <div>
-            <ProfilePost />
-          </div>
-      </div>
       <div className="footer">
         <Footer />
       </div>
